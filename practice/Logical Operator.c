a49b967b85a44dde8870e07157eac977df47e7c6
Neelkant Newra <newra008@gmail.com>
2021-11-21 20:02:36 +0530

    Create Logical Operator.c

---
+++ practice/Logical Operator.c
@@ -0,0 +1,21 @@
+// author: Neelkant Newra
+// Date: 21 Nov 2021
+// https://github.com/neelkantnewra
+// https://www.linkedin.com/in/neelkant-newra-158306173/
+
+#include <stdio.h>
+
+int main()
+{
+    // write your code here
+    int result =  5>3 || 3<1; //true ->1 , false ->0
+    printf("%d",result);
+    return 0;
+    
+    /* 
+    
+    And -> &&
+    Or -> ||
+    
+    */
+}
