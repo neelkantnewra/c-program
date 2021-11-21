f5eef6569993a1e0891b8eadff793612421b74bc
Neelkant Newra <newra008@gmail.com>
2021-11-21 19:45:54 +0530

    Create Pre and Post Increment.c

---
+++ practice/Pre and Post Increment.c
@@ -0,0 +1,21 @@
+// author: Neelkant Newra
+// Date: 21 Nov 2021
+// https://github.com/neelkantnewra
+// https://www.linkedin.com/in/neelkant-newra-158306173/
+
+#include <stdio.h>
+
+int main() //return type
+
+{
+    // write your code here
+    int i = 1;
+    int j;
+    
+    j = i++;
+    //i++; // Post Increment
+    //++i // Pre Increment
+    printf("The value of i is %d \nThe value of j is %d",i,j);
+    
+    return 0;
+}
