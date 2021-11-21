e0b7d12a4b824898deab0af4f376bcb15e6d129b
Neelkant Newra <newra008@gmail.com>
2021-11-21 19:22:17 +0530

    Create multiple input.c

---
+++ practice/multiple input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() //return type
+
+{
+    // write your code here
+    int first_number, second_number; 
+    printf("Enter 2 values \n");
+    scanf("%d %d",&first_number,&second_number);
+    int sum_val = first_number + second_number;
+    printf("The addition of %d and %d is %d",first_number,second_number,sum_val); //print formate
+    return 0;
+}
