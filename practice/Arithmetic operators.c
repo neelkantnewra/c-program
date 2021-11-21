a18d4db0ede9745557721e169a9d5594b087e46f
Neelkant Newra <newra008@gmail.com>
2021-11-21 19:29:05 +0530

    Create Arithmetic operators.c

---
+++ practice/Arithmetic operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() //return type
+
+{
+    // write your code here
+    int first_number = 9, second_number=3,answer;
+    
+    // addition
+    answer = first_number + second_number;
+    printf("The Addition of %d and %d is %d \n",first_number,second_number,answer);
+    
+    // Subtraction
+    answer = first_number - second_number;
+    printf("The Subtraction of %d and %d is %d \n",first_number,second_number,answer);
+    
+    // Multiplication
+    answer = first_number*second_number;
+    printf("The Multiplication of %d and %d is %d \n",first_number,second_number,answer);
+    
+    // Division
+    answer = first_number / second_number;
+    printf("The Division of %d and %d is %d \n",first_number,second_number,answer);
+    
+    return 0;
+}
