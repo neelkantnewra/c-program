00dd4ac8312baaa43bb1311d214ffbb8626911b9
Neelkant Newra <newra008@gmail.com>
2021-11-21 19:37:30 +0530

    Create sort hand.c

---
+++ practice/sort hand.c
@@ -0,0 +1,21 @@
+// author: Neelkant Newra
+// Date: 21 Nov 2021
+// https://github.com/neelkantnewra
+// https://www.linkedin.com/in/neelkant-newra-158306173/
+
+#include <stdio.h>
+
+int main() //return type
+
+{
+    // write your code here
+    int first_number = 9, second_number=3,answer;
+    
+    // first_number = first_number + 5;
+    // first_number += 5;
+    // first_number*=4;
+    
+    printf("%d",first_number);
+    
+    return 0;
+}
