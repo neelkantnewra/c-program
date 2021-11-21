d3a699c0012b2895b4c4c34ebf6a41552228b75a
Neelkant Newra <newra008@gmail.com>
2021-11-21 13:41:31 +0530

    Create Add two number.c

---
+++ practice/Add two number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main() {
+    // write your code here
+    float float_number = 5.5;  //primitive datatypes float,int,char
+    int first_number = 2; 
+    int second_number = 3;
+    int sum_val = first_number + second_number;
+    char word = 'y'; 
+    printf("The addition of %d and %d is %d",first_number,second_number,sum_val); //print formate
+}
