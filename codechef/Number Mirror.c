61f8230f0b1b9de89211005ab1b85c6c80a4ae87
Neelkant Newra <newra008@gmail.com>
2021-11-22 18:35:24 +0530

    Create Number Mirror.c

---
+++ codechef/Number Mirror.c
@@ -0,0 +1,13 @@
+// author: Neelkant Newra
+// Date: 21 Nov 2021
+// https://github.com/neelkantnewra
+// https://www.linkedin.com/in/neelkant-newra-158306173/
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int num;
+	scanf("%d",&num);
+	printf("%d",num);
+	return 0;
+}
