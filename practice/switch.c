c0a2028f9199ba32e68b0e35ef48bd32ce98087f
Neelkant Newra <newra008@gmail.com>
2021-12-02 20:12:52 +0530

    Create switch.c

---
+++ practice/switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// author: Neelkant Newra
+// GitHub: https://github.com/neelkantnewra
+
+int main(void) {
+	// your code goes here
+	int i = 10;
+	switch(i)
+	{
+	    case 0:
+	        printf("Zero\n");
+	        break;
+	    case 1:
+	        printf("One\n");
+	        break;
+	    case 2:
+	        printf("Two\n");
+	        break;
+	    case 3:
+	        printf("Three\n");
+	        break;
+	    default:
+	        printf("Not a Valid Number\n");
+	}
+	return 0;
+}
