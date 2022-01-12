4a50b86362cca99bf420261503a6c1dc18559cfa
neelkantnewra <newra008@gmail.com>
2022-01-12 13:46:40 +0000

    while loop added

---
+++ practice/while loop.c
@@ -0,0 +1,18 @@
+// author: Neelkant Newra
+// Date: 12 Jan 2022
+// https://github.com/neelkantnewra
+// https://www.linkedin.com/in/neelkant-newra-158306173/
+
+#include<stdio.h>
+
+// while loop 
+
+int main(){
+    int i = 0;
+    while(i<=5){
+        printf("hi \n");
+        i++;
+    }
+
+    return 0;
+}
